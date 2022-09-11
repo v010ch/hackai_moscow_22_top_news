7e4ec6233f5bd20e00012fce522196b86c37b3cf
v010ch <iakhremenko.v@gmail.com>
2022-09-12 01:38:56 +0300

    emb title features now as a class

--- 2step_embedings_titile.py
+++ 2step_embedings_titile.py
@@ -117,6 +117,99 @@ df_test  = pd.read_csv(os.path.join(DIR_DATA, 'test_extended.csv'))
 # In[12]:
 
 
+class TitleEmb:
+    def __init__(self, inp_model_name: str, inp_model_folder: str, inp_max_length: int, inp_npca_components: int) -> None:
+        """
+        Инициализация класса
+        args:
+            inp_model_name   - название модели на huggingface
+            inp_model_folder - папка модели
+            inp_max_length   - максимальная длинна текста (заголовка) для обработки
+            inp_npca_components - длина эмбеддинга после PCA (кол-во компонент)
+        return:
+        """
+        self.pre_trained_model_name   = inp_model_name
+        self.pre_trained_model_folder = inp_model_folder
+        self.max_length = inp_max_length
+        self.npca_components = inp_npca_components
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(self.pre_trained_model_name)
+        self.model = AutoModel.from_pretrained(self.pre_trained_model_name)     
+        _ = self.model.cpu()
+
+        self.pca = PCA(n_components = self.npca_components)
+        
+
+    #Mean Pooling - Take attention mask into account for correct averaging    
+    def mean_pooling(self, model_output, attention_mask: torch.Tensor) -> torch.Tensor:
+        """
+        Усреднение с учетом маски (более точное)
+        args:
+            model_output - предсказанный эмбеддинг
+            attention_mask - масска внимания для уточнения пулинга
+        return:
+            усредненный эмбеддинг с учетом маски внимания
+        """
+        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+        return sum_embeddings / sum_mask
+    
+    
+    
+    def ttl_to_emb(self, inp_text: str) -> np.ndarray:
+        """
+        Преобразование заголовка к эмбеддингу
+        args:
+            inp_text - текст (заголовок), для преобразования в эмбеддинги
+        return:
+            np.ndarray - эмбеддинг входного текста
+        """
+        encoded_input = self.tokenizer(inp_text, padding = True, truncation = True, max_length = self.max_length, return_tensors = 'pt')
+
+        #Compute token embeddings
+        with torch.no_grad():
+            model_output = self.model(**encoded_input)
+
+        sentence_embeddings = self.mean_pooling(model_output, encoded_input['attention_mask'])
+        #print('model_output', type(model_output))
+        #print('encoded_input', type(encoded_input['attention_mask']))
+        #print('sentence_embeddings', type(sentence_embeddings))
+        return sentence_embeddings[0].cpu().detach().numpy()
+
+
+
+    def make_title_emb_features(self, inp_df: pd.DataFrame, train_pca: bool) -> pd.DataFrame:
+        """
+        Добавление эмбеддингов заголовков к заданному DataFram'у
+        args:
+            inp_df - входной pd.DataFrame для преобразования
+            train_pca - необходимо ли обучать PCA или только преобразовывать
+        return:
+            pd.DataFrame - исходный DataFrame расширенный эмбеддингами заголовков
+        """
+        inp_df = inp_df[['document_id', 'true_title']]
+        #inp_df.loc[:, 'ttl_emb'] = inp_df.true_title.progress_apply(lambda x: self.ttl_to_emb(x))
+        inp_df['ttl_emb'] = inp_df.true_title.progress_apply(lambda x: self.ttl_to_emb(x))
+        
+        if train_pca:
+            self.pca.fit(inp_df.ttl_emb.to_list())
+            print('fitting pca')
+            
+        col_names = [f'tt_emb{idx}' for idx in range(self.npca_components)]
+        emb_train = pd.DataFrame(self.pca.transform(inp_df.ttl_emb.to_list()), columns = col_names)
+        
+        inp_df = pd.concat([inp_df, emb_train], axis=1)
+        inp_df.drop('ttl_emb', axis = 1, inplace = True)
+    
+        return inp_df
+
+
+# In[13]:
+
+
 #PRE_TRAINED_MODEL_NAME = 'blanchefort/rubert-base-cased-sentiment-rurewiews'
 #MODEL_FOLDER = 'ru-blanchefort-rurewiews2'
 
@@ -132,50 +225,28 @@ MODEL_FOLDER = 'sbert_large_mt_nlu_ru'
 
 MAX_LENGTH = 24
 
-
-# In[13]:
-
-
-tokenizer = AutoTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
-model = AutoModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
-
-_ = model.cpu()
+PCA_COMPONENTS = 64
 
 
 # In[14]:
 
 
-#dir(tokenizer)
+te = TitleEmb(PRE_TRAINED_MODEL_NAME, MODEL_FOLDER, MAX_LENGTH, PCA_COMPONENTS)
 
 
 # In[15]:
 
 
-#Mean Pooling - Take attention mask into account for correct averaging
-def mean_pooling(model_output, attention_mask):
-    
-    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-    
-    return sum_embeddings / sum_mask
+print('before ', df_train.shape, df_test.shape)
+df_train = te.make_title_emb_features(df_train, True)
+df_test  = te.make_title_emb_features(df_test,  False)
+print('after  ', df_train.shape, df_test.shape)
 
 
 # In[16]:
 
 
-def ttl_to_emb(inp_text: str) -> np.ndarray:
-    
-    encoded_input = tokenizer(inp_text, padding=True, truncation=True, max_length=MAX_LENGTH, return_tensors='pt')
-
-    #Compute token embeddings
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-
-    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    
-    return sentence_embeddings[0].cpu().detach().numpy()
+#df_train2 = te.make_title_emb_features(df_train[:100], True)
 
 
 # In[ ]:
@@ -190,49 +261,10 @@ def ttl_to_emb(inp_text: str) -> np.ndarray:
 
 
 
-# ## Делаем эмбеддинга из заголовков. Трейн
-
-# In[17]:
+# In[ ]:
 
 
-df_train = df_train[['document_id', 'true_title']]
 
-
-# In[18]:
-
-
-df_train['ttl_emb'] = df_train.true_title.progress_apply(lambda x: ttl_to_emb(x))
-
-col_names = [f'tt_emb{idx}' for idx in range(df_train.ttl_emb[0].shape[0])]
-emb_train = pd.DataFrame(df_train.ttl_emb.to_list(), columns = col_names)
-# In[19]:
-
-
-PCA_COMPONENTS = 64
-
-
-# In[20]:
-
-
-get_ipython().run_cell_magic('time', '', "ttl_pca = PCA(n_components = PCA_COMPONENTS)\nttl_pca.fit(df_train.ttl_emb.to_list())\n\ncol_names = [f'tt_emb{idx}' for idx in range(PCA_COMPONENTS)]\nemb_train = pd.DataFrame(ttl_pca.transform(df_train.ttl_emb.to_list()), columns = col_names)")
-
-
-# In[21]:
-
-
-df_train = pd.concat([df_train, emb_train], axis=1)
-
-
-# In[22]:
-
-
-df_train.drop('ttl_emb', axis = 1, inplace = True)
-
-
-# In[23]:
-
-
-df_train.head(3)
 
 
 # Сохраняем только эмбеддинги, без остальных признаков
@@ -242,62 +274,6 @@ df_train.head(3)
 
 df_train.to_csv(os.path.join(DIR_DATA, f'ttl_cln_emb_train_{MODEL_FOLDER}_{MAX_LENGTH}_pca{PCA_COMPONENTS}.csv'), index = False)
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# ## Выполняем тоже с тестом
-
-# In[25]:
-
-
-df_test = df_test[['document_id', 'true_title']]
-
-
-# In[26]:
-
-
-df_test['ttl_emb'] = df_test.true_title.progress_apply(lambda x: ttl_to_emb(x))
-
-
-# Сокращаем размерность
-
-# In[27]:
-
-
-#col_names = [f'tt_emb{idx}' for idx in range(df_test.ttl_emb[0].shape[0])]
-emb_test = pd.DataFrame(ttl_pca.transform(df_test.ttl_emb.to_list()), columns = col_names)
-#emb_test = pd.DataFrame(df_test.ttl_emb.to_list(), columns = col_names)
-
-
-# In[28]:
-
-
-df_test = pd.concat([df_test, emb_test], axis=1)
-
-
-# In[29]:
-
-
-df_test.drop('ttl_emb', axis = 1, inplace = True)
-
-
-# In[30]:
-
-
-df_test.shape
-
-
-# Сохраняем только эмбеддинги, без остальных признаков
 
 # In[31]:
 
